a52689669fbc78d47d38cc0cd6e4934d6ed51ccd
Allen Chen <allenttchen@gmail.com>
2023-04-06 15:41:18 -0700

    adding tests

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="winequality",
+    author="Allen Chen",
+    packages=find_packages(),
+    license="MIT",
+)
